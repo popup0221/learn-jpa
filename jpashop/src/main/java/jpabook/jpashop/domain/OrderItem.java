092c9d00e55f4fc253c4f8c1eeae37595312abea
popup0221 <popup0221@naver.com>
2024-10-30 18:50:34 +0900

    feat: 주문 상품 비즈니스 코드

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -31,12 +31,29 @@ public class OrderItem {
 	
 	private int count; // 주문 수량
 
+	//==생성 메소드==//
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItem(item);
+		orderItem.setOrderPrice(orderPrice);
+		orderItem.setCount(count);
+
+		item.removeStock(count);
+		return orderItem;
+	}
+
 	//==비즈니스 메소드==//
     public void cancel() {
 		getItem().addStock(count);
     }
 
+	//==조회 메소드==//
+
+	/**
+	 * 주문상품 전체 가격 조회
+	 */
 	public int getTotalPrice() {
 		return getOrderPrice() * getCount();
 	}
+
 }
